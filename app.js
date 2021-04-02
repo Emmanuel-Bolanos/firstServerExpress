efd19273ffb20bd8ad0ed84775687f03a634bc0e
Emmanuel-Bolanos <Emmanuel.Bolanos@theksquaregroup.com>
2021-04-01 19:21:32 -0600

    aws get successful

---
+++ app.js
@@ -0,0 +1,110 @@
+// Joi is a class that will help us to validate information
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+// Allows us to parse json objs in the body of the request
+app.use(express.json());
+const mysql = require('mysql');
+
+// validation function
+function validateNote(Note) {
+  // set schema
+  const schema = Joi.object({
+    title: Joi.string().min(1).required(),
+    // content: Joi.string().min(1).max(255).required()
+  });
+  // validate the schema requirements
+  return schema.validate(Note);
+}
+
+// DB connection
+app.use(function(req, res, next) {
+  res.locals.connection = mysql.createConnection({
+    host: '',
+    user: '',
+    password: '',
+    database: ''
+  });
+  res.locals.connection.connect();
+  next();
+})
+
+// our "db"
+// TODO mod this to work with RDS
+const Notes = [
+  {id: 1, title: "not fake note"},
+  {id: 2, title: "Note bogaloo"},
+  {id: 3, title: "the Note of notes"},
+  {id: 4, title: "a crazy note"}
+];
+
+// // Handle GET requests
+// app.get('/api/Notes', (req, res) => {
+//   res.send(Notes);
+// });
+
+// Handle GET requests for sql!
+app.get('/', (req, res) => {
+  res.locals.connection.query('SELECT * from notes', function (error, results, fields) {
+		if (error) throw error;
+		res.send(results);
+	});
+});
+
+app.get('/api/Notes/:id', (req, res) => {
+  const Note = Notes.find(element => element.id === parseInt(req.params.id));
+  if (!Note) return res.status(404).send(`Note with id ${parseInt(req.params.id)} not found`);
+  res.send(Note);
+});
+
+// // TODO query parameters (example: /?sortBy=name) are optional
+// app.get('/api/Notes/:id', (req, res) => {
+//   res.send(req.query);
+// });
+
+// Handle POST requests
+app.post('/api/Notes', (req, res) => {
+  // validate
+  const result = validateNote(req.body);
+  // communicate error if needed
+  if (result.error) return res.status(400).send(result.error.details[0].message);
+  // adding element
+  const Note = {
+    id: Notes.length + 1,
+    title: req.body.title
+  };
+  Notes.push(Note);
+  res.send(Note);
+});
+
+// Handle PUT requests
+app.put('/api/Notes/:id', (req, res) => {
+  // locate element, return 404 if not found
+  const Note = Notes.find(element => element.id === parseInt(req.params.id));
+  if (!Note) return res.status(404).send(`Note with id ${parseInt(req.params.id)} not found`);
+  // validate
+  const result = validateNote(req.body);
+  if (result.error) return res.status(400).send(result.error.details[0].message);
+  // update
+  Note.title = req.body.title;
+  // return update to client
+  res.send(Note);
+});
+
+// Handle DELETE requests
+app.delete('/api/Notes/:id', (req, res) => {
+  // locate element, return 404 if not found
+  const Note = Notes.find(element => element.id === parseInt(req.params.id));
+  if (!Note) return res.status(404).send(`Note with id ${parseInt(req.params.id)} not found`);
+  // delete
+  const idx = Notes.indexOf(Note);
+  Notes.splice(idx, 1);
+  // return deleted element
+  res.send(Note);
+});
+
+// Set environment var
+const port = process.env.PORT || 3000;
+
+// server listening for client request
+app.listen(port, () => console.log(`listening on port ${port}...`));
