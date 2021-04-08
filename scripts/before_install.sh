f15aa092aabaa30c8875d8a2a693cb996623938d
Emmanuel-Bolanos <Emmanuel.Bolanos@theksquaregroup.com>
2021-04-08 16:37:25 -0500

    update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt update
+sudo apt upgrade
 
 # Install necessary packages
-sudo apt-get install nginx git
-sudo apt install nodejs npm
+sudo apt install nginx git nodejs npm
 
 # clone git repo
 git clone https://github.com/Emmanuel-Bolanos/firstServerExpress /home/ubuntu/
